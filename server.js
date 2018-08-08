0625d5bb4ac04df648c25299c9c8696a9489320f
Ryan <ryan@bamstrategy.com>
2018-08-08 14:42:19 -0400

    Reverting back to what I had before

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const moviesController = require('./controllers/movies.js');
 
 mongoose.connect(configDB.url);
 
-app.options('*', cors());
 app.use(cors());
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-})
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/movies', moviesController());
