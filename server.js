8dda7d5dfb7068b8e1a52d6ca729b9732872ef5a
Ryan <ryan@bamstrategy.com>
2018-08-08 14:25:59 -0400

    Using corser instead of cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app        = require('express')();
 const mongoose   = require('mongoose');
-const cors       = require('cors');
+// const cors       = require('cors');
+const corser     = require('corser');
 const bodyParser = require('body-parser');
 const morgan     = require('morgan');
 
@@ -9,8 +10,7 @@ const moviesController = require('./controllers/movies.js');
 
 mongoose.connect(configDB.url);
 
-app.use(cors());
-app.options('*', cors());
+app.use(corser.create());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/movies', moviesController());
