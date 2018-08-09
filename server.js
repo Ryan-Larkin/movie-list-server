d1f7eaa4dfb4982dbf032f42a93b2dd6c23eca08
Ryan <ryan@bamstrategy.com>
2018-08-09 16:44:06 -0400

    Adding cors fix for delete

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ mongoose.connect(configDB.url);
 
 app.options('*', cors());
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/movies', moviesController());
